5f389df3334eff4a446c2ca14c21bdbc865a078c
youswim <78892152+youswim@users.noreply.github.com>
2021-11-05 21:09:47 +0900

    Add files via upload
    서버에서 상태변수 저장하고, 외부에서 post, get방식으로 변수 수정, 불러오기 가능
    
    클라이언트에서 0.5초마다 상태변수 확인해서 상태변수를 html에 append한다.

---
+++ src/main/java/com/projec/projec/protest1/domain/RoadDto.java
@@ -0,0 +1,20 @@
+package com.projec.protest1.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.json.JSONObject;
+
+@NoArgsConstructor
+@Getter
+public class RoadDto {
+
+    private String spot_nm;
+    private String spot_num;
+
+    public RoadDto(JSONObject roadJson) {
+        this.spot_nm = roadJson.getString("spot_nm");
+        this.spot_num = roadJson.getString("spot_num");
+        //JSONObject를 인자로 받는 생성자.
+        //JSONObject의 각 key값을 갖는 정보들을 빼서 저장한다.
+    }
+}
---
+++ src/main/java/com/projec/projec/protest1/domain/RoadInfoDto.java
@@ -0,0 +1,24 @@
+package com.projec.protest1.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.json.JSONObject;
+
+@NoArgsConstructor
+@Getter
+public class RoadInfoDto {
+    private int io_type;
+    private int lane_num;
+    private int vol;
+
+    public RoadInfoDto(JSONObject roadInfoJson) {
+        this.io_type = roadInfoJson.getInt("io_type");
+        this.lane_num = roadInfoJson.getInt("lane_num");
+        this.vol = roadInfoJson.getInt("vol");
+        //JSONObject를 인자로 받는 생성자.
+        //JSONObject의 각 key값을 갖는 정보들을 빼서 저장한다.
+
+    }
+
+}
