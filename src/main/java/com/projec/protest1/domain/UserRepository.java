552b5b7aa8219665e7883dcd03750d9512c3127b
youswim <78892152+youswim@users.noreply.github.com>
2021-11-11 20:33:26 +0900

    Delete src/main/java/com/projec/protest1 directory
    중복파일 삭제

--- src/main/java/com/projec/protest1/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projec.protest1.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
