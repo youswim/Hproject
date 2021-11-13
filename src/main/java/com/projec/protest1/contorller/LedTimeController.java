b3513aac2b1924eb5ca33177786c17c9135d0ec8
youswim <youswim96@gmail.com>
2021-11-13 21:59:34 +0900

    신호등의 남은 시간 데이터를 post로 받아서 서버에 저장. get요청 들어오면 신호등 남은 시간 데이터 전송
    카메라에서 받아온 영상을 웹 크기에 맞게 자동 조절

---
+++ src/main/java/com/projec/protest1/contorller/LedTimeController.java
@@ -0,0 +1,37 @@
+package com.projec.protest1.contorller;
+
+import com.projec.protest1.dto.LedTimeDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedTimeController {
+    String rec_LedTime = "1";
+    LedTimeDto ledTimeDto = new LedTimeDto(rec_LedTime);
+    String new_LedTime;
+
+    @GetMapping("/ledtime/")
+    public LedTimeDto getLedTime(){
+        return ledTimeDto;
+    }
+
+    @PostMapping("/ledtime/")
+    public String postLedTime(@RequestBody LedTimeDto new_LedTimeDto){
+        new_LedTime = new_LedTimeDto.getLedTime();
+        System.out.println("postLedTime 진입");
+        System.out.println(ledTimeDto.getLedTime());
+        System.out.println(new_LedTime);
+
+        if(!rec_LedTime.equals(new_LedTime)){
+            rec_LedTime = new_LedTime;
+            System.out.println(rec_LedTime);
+            ledTimeDto.setLedTime(rec_LedTime);
+        }
+        return "success";
+    }
+
+
+
+}
---
+++ src/main/java/com/projec/protest1/dto/LedTimeDto.java
@@ -0,0 +1,14 @@
+package com.projec.protest1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LedTimeDto {
+    public String ledTime;
+}
