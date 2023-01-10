de6e2409da0386a4b7b467e7a9a880d32d82d55c
youswim <youswim96@gmail.com>
2023-01-10 17:10:36 +0900

    fix(HomeController.java): 컨트롤러 매핑 주소 수정
    스프링 시큐리티 코드에 맞게 주소 수정

--- src/main/java/com/projec/protest1/contorller/HomeController.java
+++ src/main/java/com/projec/protest1/contorller/HomeController.java
@@ -6,26 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/p")
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/roads")
+    @GetMapping("/p/roads")
     public String roads(){
         return "/pubhtml/roads.html";
     }
 
-    @GetMapping("/roadinfo")
+    @GetMapping("/p/roadinfo")
     public String roadinfo(){
         return "/pubhtml/roadinfo.html";
     }
 
-    @GetMapping("/camera")
+    @GetMapping("/p/camera")
     public String camera(){
         return "/pubhtml/camera.html";
     }
 
-
-    @GetMapping("/admin")
+    @GetMapping("/a/admin")
     public String admin(){
         return "/admhtml/admin.html";
     }
