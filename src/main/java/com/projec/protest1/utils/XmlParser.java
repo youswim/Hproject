75794a37b95e717627413f8092af7208ee12d960
youswim <youswim96@gmail.com>
2023-01-15 22:06:59 +0900

    feat(XmlParser.java): API 서버의 비정상 응답 반환 시, 로그 기록
    요청을 보낸 URL과 반환받은 응답을 로그로 기록
    로그는 logs/main.log 에 저장

--- src/main/java/com/projec/protest1/utils/XmlParser.java
+++ src/main/java/com/projec/protest1/utils/XmlParser.java
@@ -2,6 +2,7 @@ package com.projec.protest1.utils;
 
 import com.projec.protest1.dto.RoadDto;
 import com.projec.protest1.dto.RoadInfoDto;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
@@ -9,6 +10,7 @@ import org.json.XML;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class XmlParser {
     // 사용하는 공공 교통 API는 XML로 결과를 돌려준다.
     //작업을 편하게 하기 위해서 JSON으로 변환하여 사용
@@ -17,12 +19,13 @@ public class XmlParser {
     String volInfo = "VolInfo";
 
     public List<RoadDto> formXmlToRoadDto(String url) {
-
+        List<RoadDto> roadDtoList = new ArrayList<>();
         JSONObject jsonObject = XML.toJSONObject(ur.requestXml(url));
-        checkCode(spotInfo, jsonObject);
+        if (hasErrorCode(url, spotInfo, jsonObject)) {
+            return roadDtoList;
+        }
         JSONArray jsonArray = jsonObject.getJSONObject(spotInfo).getJSONArray("row");
 
-        List<RoadDto> roadDtoList = new ArrayList<>();
 
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject tempObj = jsonArray.getJSONObject(i);
@@ -32,11 +35,12 @@ public class XmlParser {
     }
 
     public List<RoadInfoDto> fromXmlToRoadInfoDto(String url) {
-        JSONObject jsonObject = XML.toJSONObject(ur.requestXml(url));
-        checkCode(volInfo, jsonObject);
-        JSONArray jsonArray = jsonObject.getJSONObject(volInfo).getJSONArray("row");
-
         List<RoadInfoDto> roadInfoDtoList = new ArrayList<>();
+        JSONObject jsonObject = XML.toJSONObject(ur.requestXml(url));
+        if (hasErrorCode(url, volInfo, jsonObject)) {
+            return roadInfoDtoList;
+        }
+        JSONArray jsonArray = jsonObject.getJSONObject(volInfo).getJSONArray("row");
 
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject tempObj = jsonArray.getJSONObject(i);
@@ -45,7 +49,12 @@ public class XmlParser {
         return roadInfoDtoList;
     }
 
-    private void checkCode(String info, JSONObject jsonObject) {
-        System.out.println(jsonObject.getJSONObject(info).getJSONObject("RESULT").get("CODE"));
+    private boolean hasErrorCode(String url, String info, JSONObject jsonObject) {
+        String code = (String) jsonObject.getJSONObject(info).getJSONObject("RESULT").get("CODE");
+        if (code.equals("INFO-000")) {
+            return false;
+        }
+        log.warn("url:" + url + " " + "jsonObject:" + jsonObject);
+        return true;
     }
 }
