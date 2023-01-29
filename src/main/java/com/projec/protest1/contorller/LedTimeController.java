c55ccc2a9945f45ca40f09fbca86460cc5f4ae24
youswim <youswim96@gmail.com>
2023-01-29 17:45:04 +0900

    feat: light켜져있는 시간 가져오는 로직 수정
    이전:
    신호등 서버에서 주기적으로 신호등 번호와 시간을 메인 서버에 post로 보냄.
    메인 서버에서 post 요청이 들어올 때마다 가지고 있는 정보 갱신.
    사용자가 메인 서버에 신호등 상태 정보 요청하면 가지고 있는 정보 내려줌.
    현재:
    사용자가 메인 서버에 신호등 상태 요청.
    메인 서버는 신호등 서버에 신호등 상태 정보 요청.
    신호등 서버에서 받은 정보를 사용자에 내려줌
    개선점:
    사용자가 요청할 때에만 신호등 서버와 메인 서버의 통신이 일어남
    자원을 효울적으로 사용할 수 있음.
    단, 많은 사람이 신호등 정보를 동시에 요청한다면 이전 방법이 나을 수 있음.
    하지만, 신호등 정보는 순간적으로 많은 트래픽을 받지 않으므로 이와 같은 방법이 더 낫다고 판단.

--- src/main/java/com/projec/protest1/contorller/LedTimeController.java
+++ src/main/java/com/projec/protest1/contorller/LedTimeController.java
@@ -1,34 +1,23 @@
 package com.projec.protest1.contorller;
 
-import com.projec.protest1.dto.LedTimeDto;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LedTimeController {
-    String rec_LedTime = "1";
-    LedTimeDto ledTimeDto = new LedTimeDto(rec_LedTime);
-    String new_LedTime;
 
-    @GetMapping("/ledtime/")
-    public LedTimeDto getLedTime(){
-        return ledTimeDto;
+    static final String LIGHT_STATE_URL = "http://192.168.45.36:18080/ledtime";
+
+    private String request(String url) throws UnirestException {
+        Unirest.setTimeouts(0, 0);
+        return Unirest.get(url).asString().getBody();
     }
 
-    @PostMapping("/ledtime/")
-    public String postLedTime(@RequestBody LedTimeDto new_LedTimeDto){
-        new_LedTime = new_LedTimeDto.getLedTime();
-        System.out.println("postLedTime 진입");
-        System.out.println(ledTimeDto.getLedTime());
-        System.out.println(new_LedTime);
-
-        if(!rec_LedTime.equals(new_LedTime)){
-            rec_LedTime = new_LedTime;
-            System.out.println(rec_LedTime);
-            ledTimeDto.setLedTime(rec_LedTime);
-        }
-        return "success";
+    @GetMapping("/ledtime")
+    public String getLedTime() throws UnirestException {
+        // {"light_number":1,"time":3} 이 형태가 만들어져서 내려간다.
+        return request(LIGHT_STATE_URL);
     }
 }
