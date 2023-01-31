c2ea46ac8defbbe04f7668e506cfe2c461f49150
youswim <youswim96@gmail.com>
2023-01-31 23:16:09 +0900

    feat(RoadRepository.java): rid, date, time으로 RoadAll 엔티티 조회하는 기능 추가
    PK로는 조회 불가능하기 때문에 해당 코드 생성

--- src/main/java/com/projec/protest1/repository/RoadRepository.java
+++ src/main/java/com/projec/protest1/repository/RoadRepository.java
@@ -1,7 +1,32 @@
 package com.projec.protest1.repository;
 
 import com.projec.protest1.domain.RoadAll;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface RoadRepository extends JpaRepository<RoadAll, Long> {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class RoadRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public void save(RoadAll roadAll) {
+        em.persist(roadAll);
+    }
+
+    public List<RoadAll> findRoadEntities(String rid, Integer date, Integer time) {
+        return em.createQuery("select roadAll from RoadAll roadAll " +
+                        "where roadAll.roadId = :rid " +
+                        "and roadAll.date = :date " +
+                        "and roadAll.time = :time" , RoadAll.class)
+                .setParameter("rid", rid)
+                .setParameter("date", date)
+                .setParameter("time", time)
+                .getResultList();
+    }
 }
