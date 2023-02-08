5c0120fb446fbc415a26086daa889edf84b6c732
youswim <youswim96@gmail.com>
2023-02-08 23:59:33 +0900

    refactor: 정적으로 제공해도 되는 페이지들을 바로 정적으로 제공하도록 수정
    정적 제공 가능 페이지를 정적으로 수정하여 서블릿 컨테이너를 거치지 않도록 변경

--- src/main/java/com/projec/protest1/contorller/HomeController.java
+++ src/main/java/com/projec/protest1/contorller/HomeController.java
@@ -7,21 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/p/roads")
-    public String inquiryRoadList(){
-        return "/pubhtml/roads.html";
-    }
-
-    @GetMapping("/p/road-info")
-    public String inquiryRoadInfos(){
-        return "/pubhtml/roadinfo.html";
-    }
-
-    @GetMapping("/p/camera")
-    public String inquiryVideo(){
-        return "/pubhtml/camera.html";
-    }
-
     @GetMapping("/a/admin")
     public String admin(){
         return "/admhtml/admin.html";
