f6d011d93a06f09ecce681debd41015d2a6044f8
youswim <youswim96@gmail.com>
2023-02-21 23:12:17 +0900

    feat: 관리자 페이지로 이동 버튼은 관리자 로그인 시에만 나타나도록 변경
    타임리프 기능을 사용하고자 정적으로 제공하던 index.html을 template으로 이동시키고 home.html로 이름을 변경함
    타임리프와 스프링 시큐리티 통합을 위해 의존성 추가
    sec:authorize 구문을 사용하여 권한이 ADMIN인 경우에만 관리자용 버튼을 보여주도록 함

--- src/main/java/com/projec/protest1/contorller/HomeController.java
+++ src/main/java/com/projec/protest1/contorller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
     @GetMapping("/a/admin")
     public String admin(){
         return "admhtml/admin";
