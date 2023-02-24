f85074b34d9a6d573c5dbcd9445e419e7b5fc847
youswim <youswim96@gmail.com>
2023-02-24 22:16:00 +0900

    feat(RoadController.java): DB에 값이 없다면 외부 API에서 조회하도록 기능 코드 변경
    DB에 모종의 이유로 찾는 정보가 없을 수 있다.
    이런 경우는 API에서 찾아서 반환하도록 코드를 수정했음.

--- src/main/java/com/projec/protest1/service/RoadService.java
+++ src/main/java/com/projec/protest1/service/RoadService.java
@@ -38,25 +38,31 @@ public class RoadService {
     public List<RoadInfoDto> getRoadInfo(RoadInfoSearchDto risDto) throws JsonProcessingException {
 
         if (isABeforeOrSameB(getAgoDate(LocalDate.now()), risDto.getDate())) { // 5년전보다 입력받은 날짜가 최신이라면
-
-            List<Object> redisFindResults = roadRedisRepository.findRoadInfoDto(risDto); // 레디스에서 먼저 찾아보기
-            if (!redisFindResults.isEmpty()) { // 레디스에 값이 들어있다면,
-                return redisFindResults.stream() 
-                        .map(o->parseJsonToRoadInfoDto((String)o))
-                        .collect(Collectors.toList()); // 찾아온 값 객체로 변환해서 List 담아서 반환
+            List<RoadInfoDto> infoFromLocalResult = findInfoFromLocal(risDto);
+            if (infoFromLocalResult.size() != 0) {
+                return infoFromLocalResult;
             }
-
-            List<RoadInfoDto> result = roadRepository.findRoadEntities(risDto).stream()
-                    .map(RoadInfoDto::new)
-                    .collect(Collectors.toList()); // db에서 찾아옴
-            System.out.println("db에서 값 가져옴!!");
-            for (RoadInfoDto dto : result) {
-                roadRedisRepository.saveRoadInfoDto(risDto, mapper.writeValueAsString(dto));
-                System.out.println("레디스에 값 저장!");
-            }
-            return result;
         }
         return apiRequester.requestRoadVolInfo(risDto.getRid().toUpperCase(), risDto.getDate(), risDto.getTime());
+    }
+
+    private List<RoadInfoDto> findInfoFromLocal(RoadInfoSearchDto risDto) throws JsonProcessingException {
+        List<Object> redisFindResults = roadRedisRepository.findRoadInfoDto(risDto); // 레디스에서 먼저 찾아보기
+        if (!redisFindResults.isEmpty()) { // 레디스에 값이 들어있다면,
+            return redisFindResults.stream()
+                    .map(o -> parseJsonToRoadInfoDto((String) o))
+                    .collect(Collectors.toList());
+        }
+
+        List<RoadInfoDto> result = roadRepository.findRoadEntities(risDto).stream()
+                .map(RoadInfoDto::new)
+                .collect(Collectors.toList()); // db에서 찾아옴
+        System.out.println("db에서 값 가져옴!!");
+        for (RoadInfoDto dto : result) {
+            roadRedisRepository.saveRoadInfoDto(risDto, mapper.writeValueAsString(dto));
+            System.out.println("레디스에 값 저장!");
+        }
+        return result;
     }
 
     private String getAgoDate(LocalDate localDate) { // minusYears 만큼 이전의 날짜를 반환함.
