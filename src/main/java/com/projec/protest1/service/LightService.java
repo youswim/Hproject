b3d3c2781e7b6149305d9b79e0d19c0e27cc6a6b
youswim <youswim96@gmail.com>
2023-03-11 20:51:35 +0900

    feat: RabbitMQ TLS 통신 기능 추가
    1. TLS 통신을 하도록 함
    2. RabbitMQ서버의 인증서 체인을 확인하도록 함
    
    RabbitMQ에 신호등 변경 요청을 보낼 때, 누가 보냈는지 확인하기 위해 ID, PASSWD를 보낸다.
    TLS를 적용하지 않으면, 해당 정보가 그대로 노출됨.
    신호등 변경은 아무나 할 수 없어야 하므로, 정보를 암호화 할 필요가 있어서, RabbitMQ에 TLS 적용.
    또한, 서버를 믿을 수 있는지를 확인하기 위해, rootCA의 인증서를 불러와서 서버의 것과 비교하도록 만들었음

--- src/main/java/com/projec/protest1/service/LightService.java
+++ src/main/java/com/projec/protest1/service/LightService.java
@@ -2,17 +2,27 @@ package com.projec.protest1.service;
 
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.springframework.stereotype.Service;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 @Service
+@Slf4j
 public class LightService {
 
     private static final String QUEUE_NAME = "change_request_queue";
@@ -47,10 +57,56 @@ public class LightService {
     }
 
     private ConnectionFactory initConFactory() {
+
+        // TODO: keytool로 인증서를 저장할 때, 사용한 비밀번호임
+        //  실제로 서비스를 한다면, 업로드에 주의해야 하는 부분!
+        //  현재는 개발 용도이기 때문에 오픈.
+        char[] trustPassphrase = "youswim".toCharArray();
+        KeyStore tks = null;
+        try {
+            tks = KeyStore.getInstance("JKS");
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            tks.load(new FileInputStream("certification/rabbitstore"), trustPassphrase);    //keytool 명령어로 keystore만들때 입력했던 pw
+//            tks.load(new FileInputStream("certification/teststore"), trustPassphrase);    //keytool 명령어로 keystore만들때 입력했던 pw
+        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+            throw new RuntimeException(e);
+        }
+
+        TrustManagerFactory tmf = null;
+        try {
+            tmf = TrustManagerFactory.getInstance("SunX509");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            tmf.init(tks);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+
+        SSLContext c = null;
+        try {
+            c = SSLContext.getInstance("TLSv1.2");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            c.init(null, tmf.getTrustManagers(), null);
+        } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(QUEUE_ADDRESS);
+        factory.setPort(ConnectionFactory.DEFAULT_AMQP_OVER_SSL_PORT);
         factory.setUsername(ID);
         factory.setPassword(PASSWD);
+        factory.useSslProtocol(c);
         return factory;
     }
 }
