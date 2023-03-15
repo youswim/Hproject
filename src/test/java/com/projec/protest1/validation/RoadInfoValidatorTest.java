10fd08d8c2dc1b19cce8ef02a5066349d1f35b8a
youswim <youswim96@gmail.com>
2023-03-15 22:23:06 +0900

    [Test]: 사용자 입력값 검증 코드 설명 추가

--- src/test/java/com/projec/protest1/validation/RoadInfoValidatorTest.java
+++ src/test/java/com/projec/protest1/validation/RoadInfoValidatorTest.java
@@ -2,6 +2,7 @@ package com.projec.protest1.validation;
 
 import com.projec.protest1.dto.RoadInfoSearchDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -29,6 +30,7 @@ class RoadInfoValidatorTest {
         errors = new BeanPropertyBindingResult(roadInfoSearchDto, "roadInfoSearchDto");
     }
 
+    @DisplayName("정상 도로 ID 입력 -> 검증 후, 필드에러 없음")
     @Test
     public void testValidateRid_validRid() {
         roadInfoSearchDto.setRid("A-12");
@@ -36,6 +38,7 @@ class RoadInfoValidatorTest {
         assertFalse(errors.hasFieldErrors("rid"));
     }
 
+    @DisplayName("비정상 도로 ID 입력 -> 검증 후, 필드에러 있음")
     @ParameterizedTest
     @MethodSource("provideRidCases")
     public void variousValidateRidTest(String rid, String errorCode) {
@@ -47,14 +50,15 @@ class RoadInfoValidatorTest {
 
     private static Stream<Arguments> provideRidCases() {
         return Stream.of(
-                Arguments.of(null, "isNull"),
-                Arguments.of("   ", "isNull"),
-                Arguments.of("A12", "formMismatch"),
-                Arguments.of("Z-12", "noMatchAlphabet"),
-                Arguments.of("A-50", "exceedNumber")
+                Arguments.of(null, "isNull"), // 도로 ID가 null 경우
+                Arguments.of("   ", "isNull"), // 도로 ID가 공백일 경우
+                Arguments.of("A12", "formMismatch"), // 도로 ID 형식이 맞지 않을 경우
+                Arguments.of("Z-12", "noMatchAlphabet"), // 존재하지 않는 도로 알파벳인 경우
+                Arguments.of("A-50", "exceedNumber") // 존재하지 않는 도로 번호인 경우
         );
     }
 
+    @DisplayName("정상 날짜 입력 -> 검증 후, 필드에러 없음")
     @Test
     public void testValidateDate_validDate() {
         roadInfoSearchDto.setDate("20221231");
@@ -63,6 +67,7 @@ class RoadInfoValidatorTest {
     }
 
 
+    @DisplayName("비정상 날짜 입력 -> 검증 후, 필드에러 있음")
     @ParameterizedTest
     @MethodSource("provideDateCases")
     public void variousValidateDateTest(String date, String errorCode) {
@@ -73,13 +78,14 @@ class RoadInfoValidatorTest {
 
     private static Stream<Arguments> provideDateCases() {
         return Stream.of(
-                Arguments.of(null, "isNull"),
-                Arguments.of("   ", "isNull"),
-                Arguments.of("2022/12/31", "wrongDateForm"),
-                Arguments.of(getTomorrowDate(), "exceedDate")
+                Arguments.of(null, "isNull"), // 날짜가 null인 경우
+                Arguments.of("   ", "isNull"), // 날짜가 공백인 경우
+                Arguments.of("2022/12/31", "wrongDateForm"), // 날짜 형식이 잘못된 경우
+                Arguments.of(getTomorrowDate(), "exceedDate") // 미래 날짜를 입력한 경우
         );
     }
 
+    @DisplayName("정상 시간 입력 -> 검증 후, 필드에러 없음")
     @Test
     public void testValidateTime_validTime() {
         roadInfoSearchDto.setDate("20220203");
@@ -88,6 +94,7 @@ class RoadInfoValidatorTest {
         assertFalse(errors.hasFieldErrors("time"));
     }
 
+    @DisplayName("비정상 시간 입력 -> 검증 후, 필드에러 있음")
     @ParameterizedTest
     @MethodSource("provideTimeCases")
     public void variousValidateTimeTest(String date, Integer time, String errorCode) {
@@ -100,10 +107,10 @@ class RoadInfoValidatorTest {
 
     private static Stream<Arguments> provideTimeCases() {
         return Stream.of(
-                Arguments.of("20220201", null, "isNull"),
-                Arguments.of("20220201", -1, "exceedTimeRange"),
-                Arguments.of("20230201", 24, "exceedTimeRange"),
-                Arguments.of(getTodayDate(), getNowTime(), "exceedNowTime")
+                Arguments.of("20220201", null, "isNull"), // 시간이 null인 경우
+                Arguments.of("20220201", -1, "exceedTimeRange"), // 시간이 0 ~ 23을 벗어나는 경우
+                Arguments.of("20230201", 24, "exceedTimeRange"), // 시간이 0 ~ 23을 벗어나는 경우
+                Arguments.of(getTodayDate(), getNowTime(), "exceedNowTime") // 현재 시간인 경우거나, 넘어가는 경우
         );
     }
 
